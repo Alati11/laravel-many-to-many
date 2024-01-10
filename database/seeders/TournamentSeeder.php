17648752355a34a1557d42ad5eee361f60772399
Alati11 <peppealati@gmail.com>
2024-01-10 16:39:28 +0100

    ADD - TournamentSeeder

--- database/seeders/TournamentSeeder.php
+++ database/seeders/TournamentSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tournament;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TournamentSeeder extends Seeder
 {
@@ -12,6 +14,17 @@ class TournamentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tournaments = ['Reggio Calabria Challenger','Padova Challenge Open','Genova Challenge',
+        'Internazionali Italia','ATP Challenger Cortina','ATP Venezia'];
+
+        foreach($tournaments as $tournament_name) {
+
+            $tournament = new Tournament();
+
+            $tournament->name = $tournament_name;
+            $tournament->slug = Str::slug($tournament_name);
+            $tournament->save();
+
+        }
     }
 }
